57b748f385c4a0566cac28ea049db7ed5949ec9f
hmora <hmora@Hectors-MacBook-Air.local>
2021-08-08 20:38:08 -0500

    added Getters/Setters para el DTO de Region, muy necesarios para el funcionamiento de la API
    Funcionalidad: CRUD_Region
    API: customer
    Responsable: Héctor Mora

--- src/main/java/com/dws/customerservice/dto/Region.java
+++ src/main/java/com/dws/customerservice/dto/Region.java
@@ -11,4 +11,20 @@ public class Region {
         this.id = id;
         this.region = region;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
--- src/main/java/com/dws/customerservice/service/RegionServiceImp.java
+++ src/main/java/com/dws/customerservice/service/RegionServiceImp.java
@@ -3,9 +3,11 @@ package com.dws.customerservice.service;
 import com.dws.customerservice.dto.Region;
 import com.dws.customerservice.repository.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RegionServiceImp implements RegionService {
 
     @Autowired

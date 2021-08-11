c8bea989c0d782dccc92b05628e8abe0cb8b3033
hmora <hmora@Hectors-MacBook-Air.local>
2021-08-11 00:19:52 -0500

    added get, create and update repositorios
    Funcionalidad: CRUD_Region
    API: customer
    Responsable: Héctor Mora

--- src/main/java/com/dws/customerservice/repository/RegionRepository.java
+++ src/main/java/com/dws/customerservice/repository/RegionRepository.java
@@ -1,6 +1,7 @@
 package com.dws.customerservice.repository;
 
 import com.dws.customerservice.dto.Region;
+import com.dws.customerservice.dto.RespuestaApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -18,4 +19,34 @@ public class RegionRepository {
                 new BeanPropertyRowMapper<>(Region.class));
         return regions;
     }
+
+    public Region getRegion(int id) {
+        Region region =
+                jdbcTemplate.queryForObject(
+                        String.format("SELECT * FROM region WHERE id = %id ;", id)
+                        , new BeanPropertyRowMapper<>(Region.class)
+                );
+        return region;
+    }
+
+    public RespuestaApi createRegion(Region region) {
+        jdbcTemplate.update(
+                String.format("INSERT INTO region (region) VALUES ('%region')", region.getRegion())
+        );
+        RespuestaApi msg = new RespuestaApi();
+        msg.setMessage("La región ha sido registrada");
+        return msg;
+    }
+
+    public RespuestaApi updateRegion(Region region, int id) {
+        jdbcTemplate.update(
+                String.format("UPDATE region SET region = %region WHERE id = %id;"
+                                , region.getRegion()
+                                , id)
+        );
+        RespuestaApi msg = new RespuestaApi();
+        msg.setMessage("La región ha sido actualizada");
+        return msg;
+    }
+
 }
